4166599f16fb88b6155e8ecf1abbb75f3b5a54db
Heverton <hb.hevertonborges@gmail.com>
2024-12-10 18:37:32 -0300

    add routes

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { getPersonById, getPersonByQuery, personCount, postPerson } from './controllers'
+
+const server = Bun.serve({
+  async fetch(req) {
+    const url = new URL(req.url)
+
+    if (url.pathname == '/pessoas' && req.method == 'POST') return await postPerson(req)
+    if (url.pathname.startsWith('/pessoas/') == true) return await getPersonById(url)
+    if (url.href.includes('/pessoas?t=') == true) return await getPersonByQuery(url)
+    if (url.pathname == '/contagem-pessoas') return await personCount()
+  }
+})
+
+console.log(`Server running on ${server.url}`)
